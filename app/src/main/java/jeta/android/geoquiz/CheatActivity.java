440786973dd15e690bbe241abb6a224dbdc58f65
jetakasabaqi <jetakasabaqi@gmail.com>
2019-03-05 16:52:17 +0100

    Challenge Reporting Build Version Chapter 6

--- app/src/main/java/jeta/android/geoquiz/CheatActivity.java
+++ app/src/main/java/jeta/android/geoquiz/CheatActivity.java
@@ -1,9 +1,12 @@
 package jeta.android.geoquiz;
 
+import android.animation.Animator;
 import android.content.Intent;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewAnimationUtils;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -19,7 +22,7 @@ public class CheatActivity extends AppCompatActivity {
 
     private TextView mAnswerrTextView;
     private Button mShowAnswerButton;
-
+private TextView apiLevelTextView;
 
 
     @Override
@@ -30,6 +33,10 @@ public class CheatActivity extends AppCompatActivity {
         mAnswerIsTrue = getIntent().getBooleanExtra(EXTRA_ANSWER_IS_TRUE,false);
 
         mAnswerrTextView = findViewById(R.id.answer_text_view);
+
+        apiLevelTextView = findViewById(R.id.api_level_text_view);
+
+        apiLevelTextView.setText("API Level "+Build.VERSION.SDK);
 
         mShowAnswerButton = findViewById(R.id.show_answer_button);
 
@@ -43,6 +50,40 @@ public class CheatActivity extends AppCompatActivity {
                 }
 
                 setAnswerShown(true);
+
+                int cx = mShowAnswerButton.getWidth() / 2;
+                int cy = mShowAnswerButton.getHeight() /2 ;
+                float radius = mShowAnswerButton.getWidth();
+                Animator anim = null;
+                if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP) {
+                    anim = ViewAnimationUtils.createCircularReveal(mShowAnswerButton,cx,cy,radius,0);
+                    anim.addListener(new Animator.AnimatorListener() {
+                        @Override
+                        public void onAnimationStart(Animator animation) {
+
+                        }
+
+                        @Override
+                        public void onAnimationEnd(Animator animation) {
+
+                            mShowAnswerButton.setVisibility(View.INVISIBLE);
+                        }
+
+                        @Override
+                        public void onAnimationCancel(Animator animation) {
+
+                        }
+
+                        @Override
+                        public void onAnimationRepeat(Animator animation) {
+
+                        }
+                    });
+                    anim.start();
+
+                } else { mShowAnswerButton.setVisibility(View.INVISIBLE); }
+
+
             }
         });
     }
